8d5d056ce1649c778f3171d50c40c46aa202b000
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-11-06 23:19:39 +0500

    Connetc Router and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/SignIn.jsx'
+import Signup from './pages/SignUp.jsx'
+import About from './pages/About'
+import Profile from './pages/Profile.jsx'
 
 function App() {
   return (
     <>
+
+    <h1>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
