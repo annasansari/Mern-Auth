5d1177444557586025ff7179f47c0d3e0701157c
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-11-08 16:57:15 +0500

    Create test api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './router/userRouter.js';
 dotenv.config()
 
 const app = express();
@@ -16,3 +17,6 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 })
+
+
+app.use('/api/user', userRouter)
