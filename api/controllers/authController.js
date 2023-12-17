3909d4ab92c868debc42d1bb18f3a0dea083e9b9
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-12-17 15:04:42 +0500

    changes

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,22 +1,13 @@
 import User from "../model/userSchema.js"
-<<<<<<< HEAD
 import bcrypt from 'bcrypt'
 import errorHandler from "../utils/error.js";
-=======
-// import bcryptjs from 'bcryptjs'
->>>>>>> 64c7fe769b04c68daaabc22bf4286491840d1c1f
 // import errorHandler from "../utils/error.js";
 
 
 const signUp = async (req, res, next) => {
     const { username, email, password } = req.body
-<<<<<<< HEAD
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword })
-=======
-    // let hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password })
->>>>>>> 64c7fe769b04c68daaabc22bf4286491840d1c1f
     try {
         await newUser.save()
         res.status(201).json({ message: 'User Create Successfully' })
@@ -27,4 +18,4 @@ const signUp = async (req, res, next) => {
 
 
 
-export default  signUp
+export default signUp
