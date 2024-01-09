0f5618e5756f7ce6d300efd32e3b3568ecd19f80
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2024-01-09 15:22:21 +0500

    adding home and about page content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+<div>
+      <div className='mt-12 w-full flex justify-center items-center'>
+        <div className='w-full lg:w-3/4 xl:w-1/2 flex flex-col gap-3 justify-center items-center'>
+          <h1 className='text-4xl'>About page</h1>
+          <p className='text-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil reprehenderit odit voluptas cum ipsa iusto quos facilis ut, mollitia nulla, dolore nostrum officiis quo odio maxime explicabo expedita facere porro.
+          </p>
+          <p className='text-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil reprehenderit odit voluptas cum ipsa iusto quos facilis ut, mollitia nulla, dolore nostrum officiis quo odio maxime explicabo expedita facere porro.
+          </p>
+          <p className='text-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil reprehenderit odit voluptas cum ipsa iusto quos facilis ut, mollitia nulla, dolore nostrum officiis quo odio maxime explicabo expedita facere porro.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
